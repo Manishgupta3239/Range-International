707a553da178b090c5708c5e232da6dd0f68df7c
Manishgupta3239 <guptamanish3239@gmail.com>
2025-10-22 15:34:35 +0530

    changes7

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from "express"
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import { postRouter } from "./routes/postRoute.js";
 import cron from "node-cron";
 import axios from "axios";
-import  createToken  from "./lib/createToken.js";
+import createToken from "./lib/createToken.js";
+import { set } from "mongoose";
 
 dotenv.config();
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,7 +19,6 @@ let token = "";
 // middlewares
 app.use(cors());
 app.use(express.json());
-
 
 // setTimeout(() => {
 //     cron.schedule("*/10 * * * *", async() => {
@@ -34,17 +33,26 @@ app.use(express.json());
 // });
 //   }, 10000);
 
-
-
 // api
-app.use("/api/post",postRouter);
+app.use("/api/post", postRouter);
+app.get("/api/token", async (req, res) => {
+  try {
+    const res = await axios.post(
+      `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.ZOHO_REFRESH_TOKEN}&client_id=${process.env.ZOHO_CLIENT_ID}&client_secret=${process.env.ZOHO_CLIENT_SECRET}&redirect_uri=${process.env.ZOHO_REDIRECT_URI}&grant_type=refresh_token`
+    );
+    token = res.data.access_token;
+    res.status(200).json({message:"token created",token});
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({message:"token not created"});
+  }
+});
 
-
-try{
-    app.listen(PORT, ()=>{
-        console.log(`connected to ${PORT}`);
-    })
-}catch(error){
-    console.log("ERROR IN connecting",error.message);
+try {
+  app.listen(PORT, () => {
+    console.log(`connected to ${PORT}`);
+  });
+} catch (error) {
+  console.log("ERROR IN connecting", error.message);
 }
 export default token;
