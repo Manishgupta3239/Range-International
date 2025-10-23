a32c6a43cc6e014ebec04772482be9fe4c4650ea
Manishgupta3239 <guptamanish3239@gmail.com>
2025-10-23 12:01:42 +0530

    change19

--- server.js
+++ server.js
@@ -15,14 +15,15 @@ let token = "";
 
 if(token == ""){
     try {
-      const res = await axios.post(
-        "https://accounts.zoho.com/oauth/v2/token?refresh_token=1000.89df4173347843c9188e4761ebea3d23.73fc2e4cf5f43ab24d22faf014328d60&client_secret=7f606f4851e64d48437dd2a965c974e79932df8eb4&client_id=1000.Y7C03TSOIAH5MGGO422CYCMKM71VTL&redirect_uri=https://crm.zoho.in/&grant_type=refresh_token"
-      );
-      token = res.data.access_token;
-      console.log("New token:", token);
-    } catch (error) {
-      console.error("Error fetching Zoho token:", error.message);
-    }
+    const response = await axios.post(
+      `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.ZOHO_REFRESH_TOKEN}&client_id=${process.env.ZOHO_CLIENT_ID}&client_secret=${process.env.ZOHO_CLIENT_SECRET}&redirect_uri=${process.env.ZOHO_REDIRECT_URI}&grant_type=refresh_token`
+    );
+    token = response.data.access_token;
+    console.log("token",token);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({message:"token not created"});
+  }
 
 }
 // middlewares
